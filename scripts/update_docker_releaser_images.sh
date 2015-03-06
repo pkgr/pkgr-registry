4a009eaa1c20ee9ab28c2236f97b8625749b38f4
Cyril Rohr <cyril.rohr@gmail.com>
2015-03-06 13:44:09 +0000

    fix releaser

--- scripts/update_docker_releaser_images.sh
+++ scripts/update_docker_releaser_images.sh
@@ -8,7 +8,10 @@ DOCKER_DIR="${REGISTRY_DIR}/docker"
 RPMS3_DOCKER_DIR="${REGISTRY_DIR}/rpm-s3"
 REGISTRY=${REGISTRY:=""}
 
-sudo docker build -t ${REGISTRY}rpm_s3/centos:6.4 ${RPMS3_DOCKER_DIR}/
-sudo docker push ${REGISTRY}rpm_s3/centos:6.4
+sudo docker build -t rpm_s3/centos:6.4 ${RPMS3_DOCKER_DIR}/
+if [ "$REGISTRY" != "" ]; then
+	sudo docker tag rpm_s3/centos:6.4 ${REGISTRY}/rpm_s3/centos:6.4
+	sudo docker push ${REGISTRY}/rpm_s3/centos:6.4
+fi
 
 echo "DONE!"
